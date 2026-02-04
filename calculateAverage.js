010b2f0577ea8545462048b6d92c7baa6a7bbbf3
Pankaj09M <pankaj@medblocks.com>
2026-02-04 09:10:56 +0530

    Refactor calculateAverage function to correctly compute the average by dividing the sum by the array length, and added additional test cases for validation.

--- calculateAverage.js
+++ calculateAverage.js
@@ -1,10 +1,11 @@
 const calculateAverage = (arr) => {
   if (!!arr && arr?.length > 0) {
-    const ans = arr.reduce((a, b) => a + b);
-    console.log(ans);
+    const ans = arr.reduce((a, b) => a + b, 0);
+    console.log(ans / arr?.length);
   }
 };
 
 calculateAverage([1, 2, 3]);
+calculateAverage([5, 10, 2, 8]);
 calculateAverage([10, 20, 30, 100]);
 calculateAverage([12, 233, 313]);
